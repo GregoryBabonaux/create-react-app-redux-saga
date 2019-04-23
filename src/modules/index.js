5dc7fec393ad75925a485bae4874726988e9e438
Daib <Bai>
2019-04-23 13:40:42 +0200

    Upgrade dependancies forgot yarn lock ignore yarn lock file

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,12 +1,16 @@
 import { combineReducers } from 'redux';
+import { connectRouter } from 'connected-react-router';
+
 import counter from './counter/';
 import auth from './auth/';
 import dog from './dog';
 import basket from './basket';
 
-export default combineReducers({
-  counter,
-  auth,
-  dog,
-  basket,
-});
+export default history =>
+  combineReducers({
+    counter,
+    auth,
+    dog,
+    basket,
+    router: connectRouter(history),
+  });
